452213d6c512124e73199d0bb93f5527110280c7
Abdelrahman Tarek <Abdulrahmantarek0.1@gmail.com>
2025-04-20 12:16:25 +0200

    Initial commit

---
+++ src/main/java/com/masteryhub/e_commerce/ECommerceApplication.java
@@ -0,0 +1,13 @@
+package com.masteryhub.e_commerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceApplication {
+	int a;
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceApplication.class, args);
+	}
+
+}
